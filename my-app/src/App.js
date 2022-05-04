86fa5ab6dc173875ffff9980d305636a416f05ef
Sriharsha1103 <sriharsha1103@gmail.com>
2022-05-04 22:12:59 +0530

    Added Life Cycle Methods: Mounting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import DisplayArray from './components/DisplayArray/DisplayArray';
 import ChangeColor from './components/ClassComponents/ChangeColor';
 import Parent from './components/StatelessComponents/Parent';
+import Mounting from './components/ClassComponents/LifeCycle/Mounting';
 
 function App() {
   return (
     <div className="App">
       {/* <DisplayArray /> */}
       {/* <ChangeColor /> */}
-      <Parent />
+     {/*  <Parent /> */}
+     <Mounting color='white'/>
     </div>
   );
 }
