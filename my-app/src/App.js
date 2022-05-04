bd7c36828ba5f07e7a13ea69ed45687c3f334d15
Sriharsha1103 <sriharsha1103@gmail.com>
2022-05-04 20:41:43 +0530

    Added Displaying Array as Table

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import DisplayArray from './components/DisplayArray/DisplayArray';
+
+function App() {
+  return (
+    <div className="App">
+      
+    
+        <DisplayArray />
+    </div>
+  );
+}
+
+export default App;
