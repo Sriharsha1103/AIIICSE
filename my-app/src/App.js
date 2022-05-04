119b2dc2847c3d6729296ad19219a42405f3eb16
Sriharsha1103 <sriharsha1103@gmail.com>
2022-05-04 21:54:34 +0530

    Added Props and Functional Components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import DisplayArray from './components/DisplayArray/DisplayArray';
 import ChangeColor from './components/ClassComponents/ChangeColor';
+import Parent from './components/StatelessComponents/Parent';
 
 function App() {
   return (
     <div className="App">
       {/* <DisplayArray /> */}
-      <ChangeColor />
+      {/* <ChangeColor /> */}
+      <Parent />
     </div>
   );
 }
