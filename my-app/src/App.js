90b092cea3e6c129167861108a2b5e7ef80783af
Sriharsha1103 <sriharsha1103@gmail.com>
2022-05-04 21:28:45 +0530

    Added Code for State and Class components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import DisplayArray from './components/DisplayArray/DisplayArray';
+import ChangeColor from './components/ClassComponents/ChangeColor';
 
 function App() {
   return (
     <div className="App">
-      
-    
-        <DisplayArray />
+      {/* <DisplayArray /> */}
+      <ChangeColor />
     </div>
   );
 }
